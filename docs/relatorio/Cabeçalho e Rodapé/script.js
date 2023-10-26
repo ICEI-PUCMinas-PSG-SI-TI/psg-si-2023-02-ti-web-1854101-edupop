29a3774af82944fc69fb6b4869743e8d43bb84ed
diego4500 <144078438+diego4500@users.noreply.github.com>
2023-10-25 22:34:36 -0300

    Add files via upload

---
+++ docs/relatorio/Cabeçalho e Rodapé/script.js
@@ -0,0 +1,9 @@
+
+var elemento = document.querySelector('#botaoIniciar');
+
+elemento.addEventListener('click', function(){
+    var divTela = document.querySelector('#tela');
+    divTela.innerHTML = "Olá mundo";
+});
+
+
