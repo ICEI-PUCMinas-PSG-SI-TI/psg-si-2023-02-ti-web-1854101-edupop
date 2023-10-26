3e0cdbe92e495e1aca5149469c999589901cc296
diego4500 <144078438+diego4500@users.noreply.github.com>
2023-10-25 22:36:56 -0300

    Delete docs/relatorio/Cabeçalho e Rodapé directory

--- docs/relatorio/Cabeçalho e Rodapé/script.js
+++
@@ -1,9 +0,0 @@
-
-var elemento = document.querySelector('#botaoIniciar');
-
-elemento.addEventListener('click', function(){
-    var divTela = document.querySelector('#tela');
-    divTela.innerHTML = "Olá mundo";
-});
-
-
