af41d7e2788fcf7a74c63a5017dc50de38db4874
Leonardo Mendes <leomendesrch2005@gmail.com>
2023-10-29 09:18:37 -0300

    refactor: organized folder structure

---
+++ src/codigo/ModeloPágina/modelo.js
@@ -0,0 +1,14 @@
+/*
+    Para renderizar o cabeçalho e o rodapé
+    na sua pagina, deve usar a seguinte função
+    dentro da tag script no arquivo html:
+    
+    $(function () {
+            $("#tagCabecalho").load("cabecalho.html");
+            $("#tagRodape").load("rodape.html");
+        });
+
+    obs: substituir o cabecalho.html pelo diretorio correto,
+    exemplo : "../cabeçalho/cabecalho.html";
+    
+*/
